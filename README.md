93f00a150e610922cf2e3d38520196a839acd24e
R. S. Doiel <rsdoiel@gmail.com>
2015-03-02 18:09:05 -0800

    Initial setup.

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# ws-search
+
+Implement a set of commandline tools to index a static website for use by _ws_ websearch option.
+
+
---
+++ html2wordlist.go
@@ -0,0 +1,38 @@
+/**
+ * html2wordlist.go - convert the body of a HTML file into a
+ * plain text document without any HTML elements.
+ *
+ * @author R. S. Doiel, <rsdoiel@usc.edu>
+ * copyright (c) 2015 All rights reserved.
+ * Released under the Simplified BSD License.
+ */
+package main
+
+import (
+	"errors"
+	"log"
+)
+
+func findHTMLFiles(root string) ([]string, error) {
+	return nil, errors.New("findHTMLFiles() not implemented.")
+}
+
+func extractWords(fname string) ([]string, error) {
+	return nil, errors.New("extractWords() not implemented.")
+}
+
+func main() {
+
+	log.Println("Proof of concept converting an HTML document into a Wordlist")
+	fileList, err := findHTMLFiles("demo")
+	if err != nil {
+		log.Fatalf("ERROR: %s\n", err)
+	}
+	log.Printf("DEBUG %v\n", fileList)
+	for i := 0; i < len(fileList); i += 1 {
+		log.Printf("DEBUG process file no %i: %s\n", i, fileList[i])
+		words, err := extractWords(fileList[i])
+		log.Printf("DEBUG words in file: %v : %v\n", words, err)
+		// Push the new words onto the wordlist
+	}
+}
