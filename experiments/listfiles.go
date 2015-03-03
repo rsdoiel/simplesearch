91c480ba2b4a960eec8153bfe5d65123c7975752
R. S. Doiel <rsdoiel@gmail.com>
2015-03-03 07:05:54 -0800

    wrote up little file walker example.

--- experiments/listfiles.go
+++ experiments/listfiles.go
@@ -3,51 +3,30 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
-const maxFileNames = 100
-
-type DirectoryListing struct {
-	RootPathname string
-	Root         *os.FileInfo
-	Files        []os.FileInfo
-	SubFolders   []os.FileInfo
-	TotalEntries int
+func walkpath(path string, fileInfo os.FileInfo, err error) error {
+	if err != nil {
+		log.Printf("WARNING: %v\n", err)
+		return nil
+	}
+	log.Printf("DEBUG path: %s\n", path)
+	return err
 }
 
-func GetDirectoryListing(pathname string) (DirectoryListing, error) {
+func getDirectoryListing(pathname string) error {
 	dir, err := os.Open(pathname)
-	if err != nil {
-		log.Fatalf("ERROR: %v\n", err)
+	if err == nil {
+		defer dir.Close()
+		filepath.Walk(pathname, walkpath)
 	}
-	defer dir.Close()
+	log.Printf("WARNING: %v\n", err)
 
-	listing := new(DirectoryListing)
-	listing.RootPathname = pathname
-	listing.Root = dir
-	dirContents, err := dir.Readdir(maxFileNames)
-	if err != nil {
-		log.Fatalf("ERROR: %v\n", err)
-	}
-	log.Printf("DEBUG dirContents: %v\n", dirContents)
-
-	allDone := false
-	for allDone == false {
-		for i := 0; i < len(dirContents); i++ {
-			log.Printf("DEBUG %v\n", dirContents[i])
-		}
-		allDone = true
-	}
-
-	return nil, errors.New("ListDirectory() not fully implemented")
+	return err
 }
 
 func main() {
-	rootDir := "./demo/site-01"
-	log.Println("DEBUG " + rootDir)
-	for i := 0; i < len(dirContents); i++ {
-		if dirContents[i].IsDir() == true {
-			log.Printf("DEBUG %s is a directory\n", rootDir)
-		}
-	}
+	rootDir := "../"
+	getDirectoryListing(rootDir)
 }
