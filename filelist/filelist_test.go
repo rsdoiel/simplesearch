af94b0edd62894a310923d127bfbd12f14fa76ba
R. S. Doiel <rsdoiel@gmail.com>
2015-03-03 18:30:14 -0800

    Initial Sketch

--- filelist/filelist_test.go
+++ filelist/filelist_test.go
@@ -7,4 +7,10 @@ import (
 
 func main() {
 	log.Fatalf("filelist_test.go not implemented")
+	rootDir := "../filelist"
+	dirContents, err := filelist.GetDirectoryListing(rootDir)
+	if err != nil {
+		log.Fatalf("GetDirectoryListing() threw err %v\n", err)
+	}
+	log.Printf("dirContents: %v\n", dirContents)
 }
