e465287b960a62b88eac28e0450cef355902398d
R. S. Doiel <rsdoiel@gmail.com>
2015-03-03 18:26:55 -0800

    Initial Sketch

---
+++ filelist/filelist_test.go
@@ -0,0 +1,10 @@
+package filelist_test
+
+import (
+	"../filelist"
+	"log"
+)
+
+func main() {
+	log.Fatalf("filelist_test.go not implemented")
+}
---
+++ words/words_test.go
@@ -0,0 +1,10 @@
+package words_test
+
+import (
+	"../wordlist"
+	"log"
+)
+
+func main() {
+	log.Fatalf("words_test.go not implemented")
+}
