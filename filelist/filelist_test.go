9022a1148c72542ce683a14ac8bbb32c7349198c
R. S. Doiel <rsdoiel@gmail.com>
2015-03-03 20:48:25 -0800

    Initial Sketch

--- filelist/filelist_test.go
+++ filelist/filelist_test.go
@@ -2,15 +2,38 @@ package filelist_test
 
 import (
 	"../filelist"
-	"log"
+	"fmt"
+	"testing"
 )
 
-func main() {
-	log.Fatalf("filelist_test.go not implemented")
+func indexOf(target string, arrayOfStrings []string) bool {
+	for _, str := range arrayOfStrings {
+		if target == str {
+			return true
+		}
+	}
+	return false
+}
+
+func TestGetDirectoryListing(t *testing.T) {
+	expectedDirContents := []string{
+		"../filelist",
+		"../filelist/filelist.go",
+		"../filelist/filelist_test.go",
+	}
 	rootDir := "../filelist"
 	dirContents, err := filelist.GetDirectoryListing(rootDir)
 	if err != nil {
-		log.Fatalf("GetDirectoryListing() threw err %v\n", err)
+		t.Error("GetDirectoryListing() threw err %v\n", err)
 	}
-	log.Printf("dirContents: %v\n", dirContents)
+	fmt.Printf("DEBUG %v\n", dirContents)
+	if len(dirContents) != 3 {
+		t.Error("unexpected directory results: %v\n", dirContents)
+	}
+
+	for _, target := range expectedDirContents {
+		if indexOf(target, dirContents) == false {
+			t.Error("Could not find %s in %v\n", target, dirContents)
+		}
+	}
 }
